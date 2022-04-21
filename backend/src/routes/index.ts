f6710a2a964cdba0fb9c6ec824d53cb307f7b5c2
santoniriccardo <santoni.riccardo@gmail.com>
2022-04-21 11:00:06 -0400

    Introduced initial models for location, reservation, reservationRequest, and restaurant. Created routes and controller for location model

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -1,8 +1,10 @@
 import { Application } from "express";
 import userRouter from "./UserRoutes";
+import locationRouter from "./LocationRoutes";
 
 export default class Routes {
   constructor(app: Application) {
     app.use("/api/v1/users", userRouter);
+    app.use("/api/v1/users", locationRouter);
   }
 }
