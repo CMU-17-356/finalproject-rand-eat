91713664bfe72c50f769e7eb07d17746ce192462
santoniriccardo <santoni.riccardo@gmail.com>
2022-04-21 11:13:06 -0400

    Introduced reservation model, controller, and routes

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -2,11 +2,13 @@ import { Application } from "express";
 import userRouter from "./UserRoutes";
 import locationRouter from "./LocationRoutes";
 import restaurantRouter from "./RestaurantRoutes";
+import reservationRouter from "./ReservationRoutes";
 
 export default class Routes {
   constructor(app: Application) {
     app.use("/api/v1/users", userRouter);
     app.use("/api/v1/locations", locationRouter);
     app.use("/api/v1/restaurants", restaurantRouter);
+    app.use("/api/v1/reservations", reservationRouter);
   }
 }
