6b982bd6416e8796b495840da710c54d4f602aaa
Dennis Lo <dlo2@andrew.cmu.edu>
2022-05-01 19:12:18 -0400

    Add basic register and login page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,54 +1,30 @@
-import {Page, Spacer, Text, Image, Grid, Card, Button, Divider} from '@geist-ui/react';
+import { useState } from 'react';
 import './App.css';
-import restaurant from './assets/restaurant.png';
-import {Link } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { CssBaseline, Divider } from '@geist-ui/react';
+import NavComponent from './components/common/nav';
+import ListingComponent from './components/pages/listing';
+import ReservationHistoryComponent from './components/pages/reservationHistory';
+import ConfirmationComponent from './components/pages/confirmation';
+import LoginComponent from './components/pages/login';
+import FrontPageComponent from './components/pages/frontPage';
 
 function App() {
+  const [user, setUser] = useState(null);
+
   return (
-    <div className="App">
-      <div>
-        <div className='cover'>
-          <div className="container">
-            <Spacer h={2}/>
-            <Image className='home-image' src={restaurant} />
-            <h2 class='centered'>Let us find a restaurant for you based on your unique tastes.</h2>
-          </div>
-        </div>
-        
-        <div className='page'>
-          <Page>
-            <Page.Content>
-              <Spacer h={3}/>
-              <h1>Welcome to Rand.eat</h1>
-              <h3>Where great decisions are made easily!</h3>
-              <Spacer/>
-              <Grid.Container gap={2} justify="center" height="100px">
-                <Grid justify="center"xs={6}><Card justify="center"shadow width="100%">
-                  <Text>Add your preference details here and allow us to select a restaurant for you.</Text>
-                  <Divider/>
-                  <Link to="listing"><Button type="success"ghost auto scale >Go to Booking</Button></Link>
-                </Card></Grid>
-                <Grid justify="center"xs={6}><Card justify="center"shadow width="100%">
-                  <Text>Revisit your previous restaurant bookings to experience your favorite restaurants again.</Text>
-                  <Divider/>
-                  <Link to="listing"><Button type="success"ghost auto scale>Reservation History</Button></Link>
-                </Card></Grid>
-                <Grid justify="center"xs={6}><Card justify="center"shadow width="100%">
-                  <Text>Check your reservation confirmation details once you have selected your preferences. </Text>
-                  <Divider/>
-                  <Link to="confirmation"><Button type="success"ghost auto scale>Confirmation</Button></Link>
-                </Card></Grid>
-            </Grid.Container>
-            <Spacer h={8}/>
-              <Text blockquote>
-                "The best and most excited avenue
-                  I have had for choosing a restaurant" - User
-              </Text>
-            </Page.Content>
-          </Page>
-        </div>
-      </div>
-    </div>
+    <Router>
+      <CssBaseline />
+      <NavComponent user={user} />
+      <Divider />
+      <Routes>
+        <Route path="/" element={<FrontPageComponent />} />
+        <Route path="/login" element={<LoginComponent setUser={setUser} />} />
+        <Route path="/reservations" element={<ReservationHistoryComponent user={user} />} />
+        <Route path="/listing" element={<ListingComponent user={user} />} />
+        <Route path="/confirmation" element={<ConfirmationComponent user={user} />} />
+      </Routes>
+    </Router>
   );
 }
 
